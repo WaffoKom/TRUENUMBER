efa348ad302319258196be033f9a29de390fe104
DaniloWaffis <danilowaffis@gmail.com>
2025-07-18 19:50:14 +0100

    environement variable for, fronend url implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,12 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "localhost://3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");

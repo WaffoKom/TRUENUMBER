0776cc571c09c529a8e93a447cda8281eebd1fef
DaniloWaffis <danilowaffis@gmail.com>
2025-07-19 22:08:02 +0100

    feat: ajouter la page de paramètres au tableau de bord admin

---
+++ client/app/admin/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Settings from "@/components/dashboard/settings/Settings";
+export default function page() {
+  return (
+    <div>
+      <Settings />
+    </div>
+  );
+}

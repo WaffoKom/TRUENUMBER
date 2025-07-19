1d729da81e92d47b7b7b853ca5fd5a868a99b957
DaniloWaffis <danilowaffis@gmail.com>
2025-07-19 22:03:50 +0100

    fix: mettre à jour les options de cookie pour le login en rendant secure toujours vrai et en définissant sameSite sur none

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -74,8 +74,8 @@ export const login = async (req: Request, res: Response) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: true,
+        sameSite: "none",
       })
       .json({
         token,
